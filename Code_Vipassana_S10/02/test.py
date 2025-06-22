fe987601bcdc1ede045d389302436fb0ff175618
almostoutlier <alaharivirinchi123@gmail.com>
2025-06-22 14:19:06 -0400

    code vipassana season 10 2 sessions

---
+++ Code_Vipassana_S10/02/test.py
@@ -0,0 +1,32 @@
+import vertexai
+from vertexai.preview import reasoning_engines
+from vertexai import agent_engines
+import os
+import warnings
+from dotenv import load_dotenv
+load_dotenv() 
+
+
+GOOGLE_CLOUD_PROJECT = os.environ["GOOGLE_CLOUD_PROJECT"]
+GOOGLE_CLOUD_LOCATION = os.environ["GOOGLE_CLOUD_LOCATION"]
+GOOGLE_API_KEY = os.environ["GOOGLE_API_KEY"]
+GOOGLE_GENAI_USE_VERTEXAI=os.environ["GOOGLE_GENAI_USE_VERTEXAI"]
+AGENT_NAME = "adk_renovation_agent"
+MODEL_NAME = "gemini-2.5-pro-preview-03-25" 
+warnings.filterwarnings("ignore")
+PROJECT_ID = GOOGLE_CLOUD_PROJECT
+
+reasoning_engine_id = "projects/824478552869/locations/us-central1/reasoningEngines/4574364195734159360"
+
+vertexai.init(project=PROJECT_ID, location="us-central1")
+agent = agent_engines.get(reasoning_engine_id)
+print("**********************")
+print(agent)
+print("**********************")
+
+
+for event in agent.stream_query(
+    user_id="test_user",
+    message="I want you to check order status.",
+):
+    print(event)
